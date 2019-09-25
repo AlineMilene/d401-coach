2193fcde1f56b98da4488fa2b6566ec7cc16201c
aline <alinem2104@gmail.com>
2019-09-25 11:06:18 -0300

    Fazer tudo ai, desde migração até oo metódo post

--- website/models.py
+++ website/models.py
@@ -5,5 +5,11 @@ class Coach(models.Model):
     nome = models.CharField(max_length=255, verbose_name='Nome')
     frase = models.TextField()
     inspirador = models.CharField(max_length=255, null=True)
+    # null = campo não obrigatório
+    # blank = campo vazio
+
     criado_em =  models.DateTimeField(default=timezone.now)
     ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome + ' - ' + self.frase ## mostrar no admin o nome e frase 
