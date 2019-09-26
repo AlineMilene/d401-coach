b0b15ed609892e577c454787d6bcd21715ff0ef1
AE <alinem2104@gmail.com>
2019-09-26 11:57:37 -0300

    Estruturar o login

--- website/models.py
+++ website/models.py
@@ -13,3 +13,8 @@ class Coach(models.Model):
 
     def __str__(self):
         return self.nome + ' - ' + self.frase ## mostrar no admin o nome e frase 
+
+class User(models.Model):
+    nome = models.ForeignKey(Coach, on_delete=None)
+    email = models.EmailField(max_length=255, verbose_name='Email')
+    senha = models.CharField(max_length=16, verbose_name='Senha')
